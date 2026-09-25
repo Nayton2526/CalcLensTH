62ff060e038dfc0e58e279279bf6bc22a4af12a2
Nayton2526 <c.auttakorn@gmail.com>
2026-09-25 14:27:49 +0700

    V0.3 add screenshot and gallery image bridge

--- app/src/main/java/com/calclensth/app/OcrBridge.java
+++ app/src/main/java/com/calclensth/app/OcrBridge.java
@@ -3,15 +3,24 @@ package com.calclensth.app;
 import android.webkit.JavascriptInterface;
 
 public class OcrBridge {
-    public interface CameraLauncher { void launchCamera(); }
-    private final CameraLauncher launcher;
+    public interface ImageLauncher {
+        void launchCamera();
+        void pickImage();
+    }
 
-    public OcrBridge(CameraLauncher launcher) {
+    private final ImageLauncher launcher;
+
+    public OcrBridge(ImageLauncher launcher) {
         this.launcher = launcher;
     }
 
     @JavascriptInterface
     public void takePhoto() {
         launcher.launchCamera();
+    }
+
+    @JavascriptInterface
+    public void chooseImage() {
+        launcher.pickImage();
     }
 }
